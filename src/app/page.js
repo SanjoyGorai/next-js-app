3498a3ba4adc0b068a735f0c2c476656086f10d2
Mr. Sanjoy <--add>
2024-01-24 17:13:17 +0530

    login page created

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,16 @@
 import React from 'react'
+// import Login from './components/Login'
+import Login from '@/app/components/Login'
+
 
 const page = () => {
   return (
-    <div className='bg-red-300'>Sanjoy Gorai</div>
+    <>
+
+      <h1 className='text-red-700 text-2xl font-bold mx-5 p-5'>Hello</h1>
+      <Login />
+
+    </>
   )
 }
 
